279da7ddf631b493cb00313b61c86605fb2214db
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-11-02 18:14:36 +0200

    ai-smaller-model

--- ai.py
+++ ai.py
@@ -1,12 +1,19 @@
 # import tokenizer and class
 from typing import Any
-from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+# smaller model for performance
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+# larger model
+# from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+# # download and install the model
+# model_name = "facebook/blenderbot-400M-distill"
+# tokenizer: Any = BlenderbotTokenizer.from_pretrained(model_name)
+# model: tuple = BlenderbotForConditionalGeneration.from_pretrained(model_name)
 
 
 # download and install the model
-model_name = "facebook/blenderbot-400M-distill"
-tokenizer: Any = BlenderbotTokenizer.from_pretrained(model_name)
-model: tuple = BlenderbotForConditionalGeneration.from_pretrained(model_name)
+model_name = "facebook/blenderbot_small-90M"
+tokenizer: Any = AutoTokenizer.from_pretrained(model_name)
+model: tuple = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 
 async def get_bot_answer(input: str) -> Any:
@@ -19,3 +26,9 @@ async def get_bot_answer(input: str) -> Any:
     # decoding the results
     answer_txt = tokenizer.batch_decode(results)
     return answer_txt[0]
+
+
+
+
+
+
